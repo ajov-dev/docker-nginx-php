03b315f6faf9d809e72f200a12e8958381faf368
Alessandro <alessandro.olivares@utp.ac.pa>
2023-10-02 23:33:38 -0500

    update docker-compose port and servername

--- src/comment.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-/* 
- For that this to work, you need add a new host in your local machine for example in windows: C:\Windows\System32\drivers\etc\hosts.
- and add 127.0.0.1  mydomain.com .....
-*/
-
-?>    
